c72bfc7bcc13b1daa00ae2ce450aa95ce940befb
Awaken0406 <63220645+Awaken0406@users.noreply.github.com>
2023-07-07 14:53:10 +0800

    Update Login_DoIt.py

--- Login_DoIt.py
+++ Login_DoIt.py
@@ -18,6 +18,9 @@ option.add_argument('--headless')
 option.add_experimental_option('excludeSwitches',['enable-automation'])
 option.add_experimental_option('useAutomationExtension',False)
 
+user_agent =  ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')
+option.add_argument(f'user-agent={user_agent}')
+
 browser = webdriver.Chrome(option)
 browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',{
     'source' : 'Object.defineProperty(navigator,"webdriver",{get:()=>undefined})'
