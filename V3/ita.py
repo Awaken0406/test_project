57ef357bee4ae595fa5378e84f3f983823c98f2b
Awaken0406 <63220645+Awaken0406@users.noreply.github.com>
2023-07-20 18:29:25 +0800

    Update ita.py

--- V3/ita.py
+++ V3/ita.py
@@ -1,18 +1,50 @@
 import re
+import sys
 from playwright.sync_api import Playwright, sync_playwright, expect
 import time
+import requests
+from playwright.sync_api import Page
 
 USER = 'SenGeMail@163.com'
 PASSWORD = 'Qq734697554@'
-logger = {}
 
 #USER = '2428721828@qq.com'
 #PASSWORD = 'Kbh123456@'
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+
+
+# 定义API配置
+params = {
+        'num': 1,
+        'pt': 1,
+        'sep': 1,
+        'dedup':1,
+        'secret_id': 'oo28ceg7wvkkb7dcaxxx',
+        'signature': 'cqve9xl1u4vpyqq677g1a4j313qno16u',
+    }
+api = 'https://dps.kdlapi.com/api/getdps/'
+
+# 通过API获取代理
+def get_proxy():
+    global api
+    global params
+
+    r = requests.get(api,params)
+    if r.status_code == 200:
+        proxy = {
+            "server": f'http://{r.text}',
+            "username": "d2304848217",
+            "password": "9an858x3",
+            } 
+        return proxy
+    else:
+        return None
+
+
+
+
+def run(page:Page) -> None:
+ 
     js = """
         Object.defineProperties(navigator, {webdriver:{get:()=>undefined}});
         """
@@ -37,27 +69,40 @@ def run(playwright: Playwright) -> None:
     page.get_by_text("广州意大利签证申请中心").click()
     time.sleep(1)
     page.locator("#mat-select-value-3").click()
-    page.get_by_text("D Visa and Study").click()
+    page.get_by_text("SchenGen visa").click()
     time.sleep(1)
-    page.locator("#mat-select-value-3").click()
-    page.get_by_text("D Visa and Study").click()
-    page.wait_for_event('networkidle')
     time.sleep(3)
     
-    context = page.context()
-    c = re.compile('明日重置',re.S)
-    s = re.search(c,context)
+    content = page.content()
+    c = re.compile('很抱歉，目前没有可预约时段',re.S)
+    s = re.search(c,content)
     if(s != None):
-        LIMIT_FLAG_VISIT = True
-        logger.info("visit limit !!!")
-    while(True):
-        time.sleep(100)
-    page.close()
-
-    # ---------------------
-    context.close()
-    browser.close()
+        print("目前没有可预约时段")
+    else:
+       print(content)
+       while(True):
+         time.sleep(100)
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+if __name__ == "__main__":
+ with sync_playwright() as playwright:   
+    proxy = None
+    index =0
+    while(True):    
+        if(index % 2 == 0):
+            proxy = get_proxy()
+        index += 1
+        if proxy == None:
+            sys.exit()
+        print("proxy=",proxy)
+        browser = playwright.chromium.launch(headless=False,proxy=proxy)
+        context = browser.new_context()
+        page = context.new_page()
+        try:
+            run(page)
+        except Exception as e:
+           print('error',e)               
+        page.close()
+        context.close()
+        browser.close()
+        time.sleep(60 * 5)
