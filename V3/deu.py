160c71720cdfe895b9d3b91d685def7c9e320994
Awaken0406 <63220645+Awaken0406@users.noreply.github.com>
2023-07-21 18:33:16 +0800

    Create deu.py

---
+++ V3/deu.py
@@ -0,0 +1,156 @@
+import os
+import random
+import re
+import sys
+from playwright.sync_api import Playwright, sync_playwright, expect
+import time
+import pygame
+import requests
+from playwright.sync_api import Page
+import logging
+
+USER = 'SenGeMail@163.com'
+PASSWORD = 'Qq734697554@'
+
+#USER = '2428721828@qq.com'
+#PASSWORD = 'Kbh123456@'
+logger = {}
+def init_log():
+  global logger
+  logger = logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+
+# 创建控制台 handler
+  console_handler = logging.StreamHandler()
+  console_handler.setLevel(logging.DEBUG)
+
+
+# 创建文件 handler
+  file_handler = logging.FileHandler('deu_log.log')
+  file_handler.setLevel(logging.DEBUG)
+
+# 指定日志记录格式
+  formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+  console_handler.setFormatter(formatter)
+  console_handler.encoding = 'gbk'
+  file_handler.setFormatter(formatter)
+
+# 将 handler 添加到 logger 中
+  logger.addHandler(console_handler)
+  logger.addHandler(file_handler)
+
+# 定义API配置
+params = {
+        'num': 1,
+        'pt': 1,
+        'sep': 1,
+        'dedup':1,
+        'secret_id': 'oo28ceg7wvkkb7dcaxxx',
+        'signature': 'cqve9xl1u4vpyqq677g1a4j313qno16u',
+    }
+api = 'https://dps.kdlapi.com/api/getdps/'
+
+# 通过API获取代理
+def get_proxy():
+    global api
+    global params
+
+    r = requests.get(api,params)
+    if r.status_code == 200:
+        proxy = {
+            "server": f'http://{r.text}',
+            "username": "d2304848217",
+            "password": "9an858x3",
+            } 
+        return proxy
+    else:
+        return None
+
+def PlayMusic():
+  dir_path = 'D:/Users/chenyongsen/Music/'
+  file_names = [f for f in os.listdir(dir_path) if not f.startswith('!!!')]
+  name = random.choice(file_names)
+  pygame.mixer.music.load(dir_path + name)
+  pygame.mixer.music.play()
+  print(name,'playing...')
+
+
+def run(page:Page) -> None:
+ 
+    js = """
+        Object.defineProperties(navigator, {webdriver:{get:()=>undefined}});
+        """
+    page.add_init_script(js)
+    page.goto("https://visa.vfsglobal.com/chn/zh/deu/login")
+    page.wait_for_load_state("domcontentloaded")
+    time.sleep(3)
+  #  page.get_by_role("button", name="Accept Cookies").click()
+    page.get_by_placeholder("jane.doe@email.com").click()
+    page.get_by_placeholder("jane.doe@email.com").fill(USER)
+    page.get_by_placeholder("**********").click()
+    time.sleep(1)
+    page.get_by_placeholder("**********").fill(PASSWORD)
+    time.sleep(3)
+    page.get_by_role("button", name="登录").click()
+    time.sleep(3)
+    page.get_by_role("button", name="开始新的预约").click()
+    time.sleep(1)
+
+
+    page.locator("#mat-select-value-1").click()
+    page.get_by_text("德国签证申请中心 - 广州").click()
+    time.sleep(7)
+ #   page.locator("#mat-select-value-3").click()
+ #   page.get_by_text("SchenGen visa").click()
+  
+    content = page.content()
+    c = re.compile('很抱歉，目前没有可预约时段',re.S)
+    s = re.search(c,content)
+    count = 0
+    while(s != None and count < 10):
+
+        page.locator("#mat-select-value-1").click()
+        page.get_by_text("德国签证申请中心 - 南京").click()#
+        time.sleep(3)
+        page.locator("#mat-select-value-1").click()
+        page.get_by_text("德国签证申请中心 - 广州").click()
+        count +=1
+        logger.info('load count =%d',count)
+        time.sleep(8)
+        content = page.content()
+        s = re.search(c,content)
+ 
+
+    if(s != None):
+        logger.info("目前没有可预约时段")
+    else:
+       logger.info(content)
+       PlayMusic()
+       while(True):
+         time.sleep(10000)
+
+
+if __name__ == "__main__":
+ with sync_playwright() as playwright:
+    init_log()   
+    pygame.init()
+    proxy = None
+    index =0
+    while(True):    
+        #if(index % 3 == 0):
+        proxy = get_proxy()
+        index += 1
+        if proxy == None:
+            sys.exit()
+        logger.info("proxy=%s",proxy['server'])
+        browser = playwright.chromium.launch(headless=False,proxy=proxy)
+        context = browser.new_context()
+        page = context.new_page()
+        try:
+            run(page)
+        except Exception as e:
+           logger.info('error %s',e)               
+        page.close()
+        context.close()
+        browser.close()
+        time.sleep(10)
