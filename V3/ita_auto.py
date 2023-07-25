cf15834a06b1d6444363f9aa53052bbd9d02c1c7
Awaken0406 <63220645+Awaken0406@users.noreply.github.com>
2023-07-25 17:26:59 +0800

    Update ita_auto.py

--- V3/ita_auto.py
+++ V3/ita_auto.py
@@ -3,70 +3,29 @@ import datetime
 import re
 import time
 from playwright.sync_api import Playwright, sync_playwright, expect
+import auto_fill
+import mysql_db
+from auto_fill import CAccountClass
+from playwright.sync_api import Page
 
+def do_it(page:Page):
 
-class CAccountClass:
-  def __init__(self):
-    self.account = ''
-    self.password = ''
-    self.sexual  = ''
-    self.name  = ''
-    self.Sex = ''
-    self.phone = '' 
-    self.mail = ''
-    self.passport = ''
-    self.start = '' 
-    self.end = '' 
-    self.birth = ''
-    self.effective = ''
-
-
-def fill_data(page,data:CAccountClass):
-    page.get_by_placeholder("请输入名", exact=True).click()
-    page.get_by_placeholder("请输入名", exact=True).fill(data.sexual)
-    page.get_by_placeholder("请输入名.").click()
-    page.get_by_placeholder("请输入名.").fill(data.name)
-    page.locator("#mat-select-value-7").click()
-    if data.Sex == 1:
-        page.get_by_text("男性").click()
-    else:
-        page.get_by_text("女性").click()
-
-    page.locator("app-ngb-datepicker").filter(has_text="出生日期*").locator("div").nth(3).click()
-    page.get_by_role("combobox", name="Select month").select_option(str(data.birth.month))
-    page.get_by_role("combobox", name="Select year").select_option(str(data.birth.year))
-    page.get_by_text(str(data.birth.day), exact=True).click()
-    
-    page.locator("#mat-select-value-9").click()  
-    page.get_by_text("中国").click()
-
-    page.get_by_placeholder("输入护照号").click()
-    page.get_by_placeholder("输入护照号").fill(data.passport)
-    #page.locator(".col-12 > app-input-control > div > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix").first.click()
-    page.get_by_placeholder("44").fill("86")
-    page.get_by_placeholder("012345648382").click()
-    page.get_by_placeholder("012345648382").fill(data.phone)
-    page.get_by_placeholder("输入邮箱地址").click()
-    page.get_by_placeholder("输入邮箱地址").click()
-    page.get_by_placeholder("输入邮箱地址").fill(data.mail)
-
-
-    page.locator("app-ngb-datepicker").filter(has_text="护照有效期*").locator("div").nth(3).click()
-    page.get_by_role("combobox", name="Select month").select_option(str(data.effective.month))
-    page.get_by_role("combobox", name="Select year").select_option(str(data.effective.year))
-    #page.get_by_text(str(data.effective.day), exact=True).click()
-    page.get_by_role("gridcell", name=str(data.effective.strftime("%A, %B %d, %Y"))).get_by_text(str(data.effective.day)).click()
-
-    """ 
-    page.get_by_role("button", name="保存").click()
-    page.get_by_role("button", name="继续").click()
-    page.get_by_text("24日").click()
-    page.get_by_role("cell", name="9649702 选择").get_by_label("选择选择").check()
-    page.get_by_role("button", name="继续").click()
-    page.locator("label").filter(has_text="我接受").click()
-    page.locator("#mat-checkbox-2 > .mat-checkbox-layout > .mat-checkbox-inner-container").click()
-    page.get_by_role("button", name="确认").click()
-    """
+      #<div class="alert alert-info border-0 rounded-0"> 最早可预约的时间 : 25-07-2023 </div>
+       pattern = re.compile(r'<div class="alert alert-info border-0 rounded-0"> 最早可预约的时间 : (.*?) </div>',re.S)
+       match = re.search(pattern, page.content())
+       date = match.group(1)
+       print(date)
+       time_date = datetime.datetime.strptime(date, "%d-%m-%Y").date()
+       print("日期:%s",time_date)
+       page.get_by_role("button", name="继续").click()
+       time.sleep(3)
+       data = mysql_db.GetAccount(time_date)
+       if(date == None):
+           return
+       print(vars(data))
+       auto_fill.fill_data(page, data)
+       while(True):
+         time.sleep(10000)
 
 def run(playwright: Playwright, data:CAccountClass) -> None:
     browser = playwright.chromium.launch(headless=False)
@@ -93,10 +52,10 @@ def run(playwright: Playwright, data:CAccountClass) -> None:
     page.get_by_text("科学研究").click()
     time.sleep(5)
     #page.get_by_text("最早可预约的时间 : 25-07-2023").click()
-    page.get_by_role("button", name="继续").click()
-    time.sleep(8)
+   # page.get_by_role("button", name="继续").click()
+   # time.sleep(8)
     try:
-        fill_data(page,data)
+        do_it(page)
     except Exception as e:
         print(e)
 
@@ -126,7 +85,6 @@ def read_config():
     data.end = datetime.datetime.strptime(config['end'], "%Y-%m-%d").date()
     data.birth = datetime.datetime.strptime(config['birth'], "%Y-%m-%d").date()
     data.effective = datetime.datetime.strptime(config['effective'], "%Y-%m-%d").date()#有效期
-    data.start.day
     return data
 
 
