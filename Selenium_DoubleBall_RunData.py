5665860d69c6f87cdae9a4fcd8ae599aed0430a6
Awaken0406 <63220645+Awaken0406@users.noreply.github.com>
2024-12-13 18:14:29 +0800

    Update Selenium_DoubleBall_RunData.py

--- Selenium_DoubleBall_RunData.py
+++ Selenium_DoubleBall_RunData.py
@@ -17,57 +17,87 @@ import string
 import Selenium_DoubleBall
 
 
-def Test(count,redTopKeys,recommendCount,BallDataList):
+
+def Test(count,recommendCount,BallDataList,redTopKeys,blueTopKeys):
 
     legth = len(BallDataList)
     totalMoney = 0
-    GroupCount = 0
-    for i in range(legth - 50, legth):#只遍历50期
-          #sleep(0.1)
-          random.seed(int(time.time() * 1000000))
+    winCount = 0
+    for i in range(legth - G_GroupCount, legth):
+          #seed = int(time.time() * 10000000)
+          #seed = 17339911835903144
+          #seed = 17339917270329938
+          #random.seed(seed)
+          
           sliced_list = BallDataList[:i]
-          #redTopKeys = Analyse(sliced_list)
-          fileName = f'./OutPut/DoubleBall_{i}.txt'
-          AllDataList = Selenium_DoubleBall.DoRecommend(G_exRed,G_exBlue,recommendCount,fileName,sliced_list,redTopKeys,False,False)
+          AllDataList = Selenium_DoubleBall.DoRecommend(redTopKeys,blueTopKeys,G_exRed,G_exBlue,recommendCount,sliced_list,False,False)
           nextData = BallDataList[i]
           #print('nextID',nextData.ID)
           money = Selenium_Recommend_Analyse.Doit(AllDataList,nextData.red,[nextData.blue])
           totalMoney += money
-          GroupCount += 1
-    totalCost = recommendCount*GroupCount*G_cost
-    print(f'第{count}次循环','推测期数:',GroupCount,'每期组数:',recommendCount, '花费:',totalCost,'中奖金额:',totalMoney)
-    return totalMoney,totalCost
+          if(money >= recommendCount*G_cost):
+              winCount += 1
+          
+    totalCost = recommendCount*G_GroupCount*G_cost
+    isSuc = winCount >= int(G_GroupCount/2)
+    print(f'{count} GroupCount:{G_GroupCount}, recommendCount:{recommendCount}, cost:{totalCost}, hit:{totalMoney}')
+    return totalMoney,totalCost,isSuc
+
+
+
+def CulcComb(red_chosen,blue_chosen):
+    #count = C(m, 6) * C(n, 1)
+    red_combinations = math.comb(red_chosen, 6)
+    blue_combinations = math.comb(blue_chosen, 1)
+    total_bets = red_combinations * blue_combinations
+
+    return total_bets
+
 
 
 if __name__ == "__main__":
 
     BallDataList = []
-    AllDataMap = Selenium_Result_Update.GetFileDate('2023-01-01')
+    AllDataMap = Selenium_Result_Update.GetFileDate('2022-01-01')#recommend number date
     for data in AllDataMap.values():
          BallDataList.append(data)
-
-    redTopKeys = Selenium_DoubleBall.Analyse(BallDataList)
-
+    redTopKeys,blueTopKeys = Selenium_DoubleBall.Analyse(BallDataList)
+   
     #Test Recommend
-    recommendCount = 10 #推荐组数
-    times = 100
+    recommendCount = 3
+    times = 10000
+    G_exRed = 2
+    G_exBlue = 2 
+    G_GroupCount = 50
+    G_cost = CulcComb(6+G_exRed,1+G_exBlue)*2
     allTotalMoney = 0
     alltotalCost = 0
+    #seed = int(time.time() * 10000000)
+    ##seed = 17339986740234198
+    #random.seed(seed)
 
-    G_exRed = 4#加N个红球
-    G_exBlue = 2#加N个篮球
-    G_cost = 168#每注多少钱
 
-    for i in range(times):
-       start_time = time.perf_counter()
-       totalMoney,totalCost = Test(i,redTopKeys,recommendCount,BallDataList)
+    seedList = []
+    #(7,1)=7, (8,1)=28, (8,2)=56 (8,3)=64
+    i=0
+    #for i in range(times):
+    while(True):
+       i += 1
+       seed = int(time.time() * 10000000)
+       random.seed(i)
+       #start_time = time.perf_counter()
+       totalMoney,totalCost,isSuc = Test(i,recommendCount,BallDataList,redTopKeys,blueTopKeys)
        allTotalMoney += totalMoney
        alltotalCost += totalCost
-       end_time = time.perf_counter()
-       execution_time = end_time - start_time
+       #end_time = time.perf_counter()
+       #execution_time = end_time - start_time
        #print(f"time:{execution_time:.6f}")
-    avgCost = alltotalCost / times
-    avgWin = allTotalMoney / times
-    print('循环次数:',times,'平均花费:',avgCost,'平均赚:',avgWin)
+       if isSuc:
+            print('!!11suc!!!! seed:%d',i)
+            break
+    avgCost = alltotalCost / times / recommendCount / G_GroupCount
+    avgWin = allTotalMoney / times / recommendCount / G_GroupCount
+    print('loopTimes:',times,'avgCost:',avgCost,'avgWin:',avgWin)
+
     
      
